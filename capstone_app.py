4f42c47eefbd1a6ec51fe34218aaff3e79305f0a
bill-kwok <102035359+bill-kwok@users.noreply.github.com>
2022-03-28 15:21:01 +0100

    Update capstone_app.py

--- capstone_app.py
+++ capstone_app.py
@@ -4,4 +4,4 @@ import pandas as pd
 df = pd.read_csv("data_for_first_two_cards.csv")
 
 st.header("Welcome!")
-st.text("Are you going to play Texas Hold'em Poker?")
+st.subheader("Are you going to play Texas Hold'em Poker?")

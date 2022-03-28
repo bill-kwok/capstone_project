8b66292bdf1940e7b59a3aa20caa466e3f0a14b0
bill-kwok <102035359+bill-kwok@users.noreply.github.com>
2022-03-28 15:38:44 +0100

    Update capstone_app.py

--- capstone_app.py
+++ capstone_app.py
@@ -10,10 +10,12 @@ with st.sidebar:
   table = st.checkbox("Table")
   
   
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(2)
 with col1:
   game = st.button("Start a new game")
 
 with col2:
   if table:
     df
+with col3:
+  st.text("test")

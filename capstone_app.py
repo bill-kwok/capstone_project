aaaad3d2cd7b79122a208a36fa519dff9b1c6772
bill-kwok <102035359+bill-kwok@users.noreply.github.com>
2022-03-28 15:35:22 +0100

    Update capstone_app.py

--- capstone_app.py
+++ capstone_app.py
@@ -5,3 +5,15 @@ df = pd.read_csv("data_for_first_two_cards.csv")
 
 st.header("Welcome!")
 st.subheader("Are you going to play Texas Hold'em Poker?")
+
+with st.sidebar:
+  table = st.checkbox("Table")
+  
+  
+col1, col2 = st.columns(2)
+with col1:
+  game = st.button(Start a new game)
+
+with col2:
+  if table:
+    df

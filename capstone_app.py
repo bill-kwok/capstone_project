e7a6578e51c2a8dbe2112c9f30c5be0145e1a00f
bill-kwok <102035359+bill-kwok@users.noreply.github.com>
2022-03-28 15:50:34 +0100

    Update capstone_app.py

--- capstone_app.py
+++ capstone_app.py
@@ -5,6 +5,21 @@ df = pd.read_csv("data_for_first_two_cards.csv")
 
 st.header("Welcome!")
 st.subheader("Are you going to play Texas Hold'em Poker?")
+placeholder = st.empty()
+
+# Replace the placeholder with some text:
+placeholder.text("Hello")
+
+# Replace the text with a chart:
+placeholder.line_chart({"data": [1, 5, 2, 6]})
+
+# Replace the chart with several elements:
+with placeholder.container():
+     st.write("This is one element")
+     st.write("This is another")
+
+# Clear all those elements:
+placeholder.empty()
 
 with st.sidebar:
   table = st.checkbox("Table")

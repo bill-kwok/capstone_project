5057d1d3d852ad28f2c6d2ef21b97b20de4b50d4
bill-kwok <102035359+bill-kwok@users.noreply.github.com>
2022-03-29 09:29:36 +0100

    Update capstone_app.py

--- capstone_app.py
+++ capstone_app.py
@@ -29,64 +29,39 @@ with st.sidebar:
   pie_size = st.slider("Size of pie chart", 1, 10, 5)
 
 col1, col2 = st.columns([1,3])
+
 with col1:
-  remaining_deck = deck.copy()
-  my_cards = set({})
-  community = set({})
-  
-  selection1 = st.multiselect("Which 2 cards have you got:", remaining_deck)
-  enter1 = st.button("Confirm the 2 cards")
-  if enter1:
-    a = 1
-  
-  st.text("")
-  selection2 = st.multiselect("The flop: first 3 community cards", remaining_deck)
-  enter2 = st.button("Confirm the first 3 community cards")
-  
-  st.text("")
-  selection3 = st.selectbox("The turn: 4th community card", remaining_deck)
-  enter3 = st.button("Confirm the 4th community card")
-  
-  st.text("")
-  selection4 = st.selectbox("The river: 5th community card", remaining_deck)
-  enter4 = st.button("Confirm the 5th community card")
-  
-  
   game = st.button("Start a new game")  
   if game:
     remaining_deck = deck.copy()
     my_cards = set({})
     community = set({})
-    
-with col2:
-  remaining_deck = deck.copy()
-  my_cards = set({})
-  community = set({})
   
   with st.form('selection1', clear_on_submit = True):
     selection1 = st.multiselect("Which 2 cards have you got:", remaining_deck)
     enter1 = st.form_submit_button("Confirm the 2 cards")
     if enter1:
-      a = 1
-  
-  st.text("")
+      st.write(selection1)
+      
   with st.form('selection2', clear_on_submit = True):
     selection2 = st.multiselect("The flop: first 3 community cards", remaining_deck)
     enter2 = st.form_submit_button("Confirm the first 3 community cards")
+    if enter2:
+      st.write(selection2)
   
-  st.text("")
   with st.form('selection3', clear_on_submit = True):
     selection3 = st.selectbox("The turn: 4th community card", remaining_deck)
     enter3 = st.form_submit_button("Confirm the 4th community card")
+    if enter3:
+      st.write(selection3)
   
-  st.text("")
   with st.form('selection4', clear_on_submit = True):
     selection4 = st.selectbox("The river: 5th community card", remaining_deck)
     enter4 = st.form_submit_button("Confirm the 5th community card")
+    if enter4:
+      st.write(selection4)
+      
+with col2:
   
   
-  game = st.button("Start a new game")  
-  if game:
-    remaining_deck = deck.copy()
-    my_cards = set({})
-    community = set({})
+

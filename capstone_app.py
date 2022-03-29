0e6e1c2d2baab643b8293492c3a5a379a995486d
bill-kwok <102035359+bill-kwok@users.noreply.github.com>
2022-03-29 10:59:30 +0100

    Update capstone_app.py

--- capstone_app.py
+++ capstone_app.py
@@ -37,37 +37,34 @@ with st.sidebar:
 col1, col2 = st.columns([1,3])
 
 with col1:
-  with st.form('my_key', clear_on_submit=True):
-    remaining_deck, my_cards, community, flop, turn, river = reset_game()
-    game = st.form_submit_button("Start a new game")  
+  remaining_deck, my_cards, community, flop, turn, river = reset_game()
+  game = st.button("Start a new game")
+  if game:
+    st.text("untick all your choices and you can start a new game.")
     
-    with st.form('form1'):
-      selection1 = st.multiselect("Which 2 cards have you got:", remaining_deck)
-      enter1 = st.checkbox("Confirm the 2 cards")
-      if enter1:
-        my_cards.update(set(selection1))
-      st.write(my_cards)
+  selection1 = st.multiselect("Which 2 cards have you got:", remaining_deck)
+  enter1 = st.checkbox("Confirm the 2 cards")
+  if enter1:
+    my_cards.update(set(selection1))
+    st.write(my_cards)
 
-    selection2 = st.multiselect("The flop: first 3 community cards", remaining_deck)
-    enter2 = st.checkbox("Confirm the first 3 community cards")
-    if enter2:
-      flop = selection2.copy()
-    st.write(flop)
+  selection2 = st.multiselect("The flop: first 3 community cards", remaining_deck)
+  enter2 = st.checkbox("Confirm the first 3 community cards")
+  if enter2:
+    flop = selection2.copy()
+  st.write(flop)
 
-    selection3 = st.selectbox("The turn: 4th community card", remaining_deck)
-    enter3 = st.checkbox("Confirm the 4th community card")
-    if enter3:
-      turn = selection3
-    st.write(turn)
+  selection3 = st.selectbox("The turn: 4th community card", remaining_deck)
+  enter3 = st.checkbox("Confirm the 4th community card")
+  if enter3:
+    turn = selection3
+  st.write(turn)
 
-    selection4 = st.selectbox("The river: 5th community card", remaining_deck)
-    enter4 = st.checkbox("Confirm the 5th community card")
-    if enter4:
-      river = selection4
-    st.write(river)
-
-    if game:
-      enter1, enter2, enter3, enter4 = False, False, False, False      
+  selection4 = st.selectbox("The river: 5th community card", remaining_deck)
+  enter4 = st.checkbox("Confirm the 5th community card")
+  if enter4:
+    river = selection4
+  st.write(river)
     
 with col2:
   if table:
